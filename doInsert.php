ef9e065a708a78933c5a8af8d1ba7007ebf9d1a6
snowbaka <55972566+snowbaka@users.noreply.github.com>
2019-10-09 15:40:07 +0700

    Create doInsert.php

---
+++ doInsert.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Processing</title>
+</head>
+<body>
+	<?php 
+		$name = $_POST["txtName"];
+		$category = $_POST["cbCategory"];	
+		$stock = $_POST["stock"];
+		$price = $_POST["txtPrice"]
+		echo $name;
+		
+		//Refere to database 
+	   $db = parse_url(getenv("DATABASE_URL"));
+	   $pdo = new PDO("pgsql:" . sprintf(
+	        "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+	        $db["host"],
+	        $db["port"],
+	        $db["user"],
+	        $db["pass"],
+	        ltrim($db["path"], "/")
+	   ));
+	   $data = [
+		    'name' => $name,
+		    'category' => $category,
+		    'stock' => $stock,
+		    'price' => $price
+		];
+		$stmt =  $pdo->prepare("INSERT INTO insertproduct(name, category, stock, price) VALUES 
+			(:name,:category,:stock; :price)");	
+		$stmt->execute($data);
+	 ?>
+	 <h2>Inserted product <?php echo $name?> into the 
+	 		<?php echo $category?> category
+	 </h2>
+	 <ul>
+	 	<li><?php echo $name?></li>
+	 	<li><?php echo $category?></li>
+	 	<li><?php echo $stock?></li>
+	 	<li><?php echo $price?></li>
+	 </ul>
+	 <a href="index.php">Index</a>
+</body>
+</html>
