bf503616639672a7d937a924987fd41d230a6eb8
snowbaka <55972566+snowbaka@users.noreply.github.com>
2019-11-01 14:15:36 +0700

    Update db.php

--- db.php
+++ db.php
@@ -1,45 +1,43 @@
 <?php 
-$hostname = 'localhost';
-$username='root';
-$password='';
-$dbname='bluebird';
-$port=3306;
+
 function query($sql)
 {
-	global $hostname;
-	global $username;
-	global $password;
-	global $dbname;
-	global $port;
-	$conn = new mysqli($hostname, $username, $password, $dbname, $port);
-	if($conn->connect_error)
-	{
-		echo "Connection fail<br>";
-		die($conn->connect_error);
-	}
-	$result = $conn->query($sql);
-	if(!$result)
-	{
-		echo "SQL execution fail <br>";
-		die($conn->error);
-	}
-	$rows = mysqli_fetch_all($result);
-	return $rows;
+	$db = parse_url(getenv("DATABASE_URL"));
+	$pdo = new PDO("pgsql:" . sprintf(
+	    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+	    $db["host"],
+	    $db["port"],
+	    $db["user"],
+	    $db["pass"],
+	    ltrim($db["path"], "/")
+	));
+	//you sql query
+	$sql = "SELECT studentname, course FROM registercourse";
+
+	$stmt = $pdo->prepare($sql);
+	//execute the query on the server and return the result set
+	$stmt->setFetchMode(PDO::FETCH_ASSOC);
+	$stmt->execute();
+	$resultSet = $stmt->fetchAll();
 }
 function execsql($sql)
 {
-	global $hostname;
-	global $username;
-	global $password;
-	global $dbname;
-	global $port;
-	$conn = new mysqli($hostname, $username, $password, $dbname, $port);
-	if($conn->connect_error)
-	{
-		echo "Connection fail<br>";
-		die($conn->connect_error);
-	}
-	$result = $conn->query($sql);
-	return $result;
+	$db = parse_url(getenv("DATABASE_URL"));
+	$pdo = new PDO("pgsql:" . sprintf(
+	    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+	    $db["host"],
+	    $db["port"],
+	    $db["user"],
+	    $db["pass"],
+	    ltrim($db["path"], "/")
+	));
+	//you sql query
+	$sql = "SELECT studentname, course FROM registercourse";
+
+	$stmt = $pdo->prepare($sql);
+	//execute the query on the server and return the result set
+	$stmt->setFetchMode(PDO::FETCH_ASSOC);
+	$stmt->execute();
+	$resultSet = $stmt->fetchAll();
 }
 ?>
